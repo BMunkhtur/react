7457dd76f84421f084d17449ddea4a6779c20e93
BMunkhtur <116338079+BMunkhtur@users.noreply.github.com>
2023-01-03 13:12:39 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
   const users = [
-    { name: "naraa", age : 23},
-    { name: "saraa", age : 26},
-    { name: "bataa", age : 24},
-    { name: "ganaa", age : 24},
-    { name: "enkhee", age : 24}
-  ]
-  let sum = 0;
-  let i = 0;
-  while (i <users.length) {
-    sum += users[i].age;
-    i++;
-  }
+    {
+      name: "Alice",
+      age: 16,
+      imageUrl:
+        "https://images.unsplash.com/photo-1672646856394-ec0dd6a4ccec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    },
+    {
+      name: "Bob",
+      age: 45,
+      imageUrl:
+        "https://images.unsplash.com/photo-1672646856394-ec0dd6a4ccec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    },
+    {
+      name: "Carl",
+      age: 5,
+      imageUrl:
+        "https://images.unsplash.com/photo-1672646856394-ec0dd6a4ccec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    },
+    {
+      name: "Dean",
+      age: 19,
+      imageUrl:
+        "https://images.unsplash.com/photo-1672646856394-ec0dd6a4ccec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    },
+    {
+      name: "Ethan",
+      age: 44,
+      imageUrl:
+        "https://images.unsplash.com/photo-1672646856394-ec0dd6a4ccec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    },
+  ];
+  // nasnii dundaj oloh
+  let sumAge = users.reduce((prev, cur) => (prev += cur.age), 0);
+  let avgAge = sumAge / users.length;
+  
   return (
-    <div className="App">
-    <h1>Hun</h1>
-    <p>{users[0]}</p>
+    <div >
+      <h1 className="avg">Average: {avgAge}</h1>
+      <div className="row">
+      {
+      users.map((user) => {
+        return (
+          <div className="card">
+            <div className="col">
+              <img src={user.imageUrl} alt="zurag" />
+            </div>
+            <div className="col">
+              <h2>Name: {user.name}</h2>
+              <p>Age: {user.age}</p>
+            </div>
+          </div>
+        );
+      })}
+      </div>
     </div>
   );
 }
-
 export default App;
