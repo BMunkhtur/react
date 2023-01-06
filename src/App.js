1fa51348ca6d6b6c6b43b1854ea6a50598eec0bf
Munkhtur <bayarsaikhanmunkthur@gmail.com>
2023-01-06 16:57:27 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Home from "./pages/homepage/Home";
 import Detail from "./pages/diidi/Detail";
+import "../src/App.scss";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Detail />
+      {/* <Home />
+      <Detail /> */}
+      <Button variant="primary">click</Button>
     </div>
   );
 }
