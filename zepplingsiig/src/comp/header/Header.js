28c5d06de8cd112dfff00e4a9a00b9e58d8fe961
BMunkhtur <bayarsaikhanmunkhtur@gmail.com>
2023-01-07 20:25:11 +0800

    up

---
+++ zepplingsiig/src/comp/header/Header.js
@@ -0,0 +1,12 @@
+// import "./navbar/Navbar"
+import Headermain from "./headermain/headermain";
+import Navbar from "./navbar/Navbar";
+const Header = () => {
+  return (
+    <div>
+      <Navbar />
+      <Headermain />
+    </div>
+  );
+};
+export default Header;
---
+++ zepplingsiig/src/comp/header/headermain/design/Design.js
@@ -0,0 +1,18 @@
+const DesignBtn = ({ design }) => {
+  const darah = (name) => {
+    alert("Hello" + " " + name);
+  };
+  return (
+    <button
+      id={`${design.name}`}
+      className="designBtn"
+      onClick={() => {
+        darah(design.name);
+      }}
+    >
+      <img className="titleimg" src={`${design.imageURL}`} alt="" />
+      <div className="title">{design.name}</div>
+    </button>
+  );
+};
+export default DesignBtn;
